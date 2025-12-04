f3512efb4cd38ee71036c7b9a583db659b2882a5
João Martins <joaommn1998@gmail.com>
2025-12-03 23:01:05 -0300

    fix: correct route matching logic in AuthMiddleware for public and token app routes

--- qqtech9-backend/src/middleware/auth-middleware.js
+++ qqtech9-backend/src/middleware/auth-middleware.js
@@ -12,20 +12,20 @@ const tokenAppRoutes = [
 
 export const AuthMiddleware = async (req, res, next) => {
   const isPublic = publicRoutes.some(
-    route => route.method === req.method && req.originalUrl.startsWith(route.path)
+    route => route.method === req.method && req.path === route.path
   )
 
   if(isPublic){
     return next();
   }
 
-  /*const isTokenAppRoute = tokenAppRoutes.some(
-    route => route.method === req.method && req.originalUrl.startsWith(route.path)
-  )
+  const isTokenAppRoute = tokenAppRoutes.some(
+    route => route.method === req.method && req.path === route.path
+  );
 
   if(isTokenAppRoute){
     return next();
-  }*/
+  }
 
   const authHeader = req.headers.authorization;
 
